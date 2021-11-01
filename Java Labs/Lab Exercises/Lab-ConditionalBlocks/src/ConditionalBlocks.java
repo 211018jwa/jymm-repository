195cf7490d0096bda08c2b33537b6dc4064a60c5
Jymm Allexzydd Enriquez <jymm.enriquez@gmail.com>
2021-11-01 14:04:14 -0500

    combined Lab #7 and Lab #8

--- Java Labs/Lab Exercises/Lab-ConditionalBlocks/src/ConditionalBlocks.java
+++ Java Labs/Lab Exercises/Lab-ConditionalBlocks/src/ConditionalBlocks.java
@@ -2,13 +2,27 @@
 public class ConditionalBlocks {
 
 	public static void main(String[] args) {
-		// boolean b = false;
-		boolean b = true;
+		// boolean b = false; //Lab #7
+		// boolean b = true; //Lab #7
 
-		if (b) {
-			System.out.println("inside the if-statement");
+		//boolean firstCondition = true; // Lab #8
+		boolean firstCondition = false; // Lab #8
+		boolean secondCondition = true; // Lab #8
+
+		if (firstCondition) { // Lab #7 ----> added firstCondition for Lab #8
+			System.out.println("inside the if-statement"); // Lab #7
+			
+			if (secondCondition) { // Lab #8
+				System.out.println("inside the nested if-statement"); // Lab #8
+			}
+		}else if (secondCondition) { // Lab #8
+			System.out.println(5); // Lab #8
+		} else { // Lab #7 ----> added else {} for Lab #8
+			System.out.println("inside of the else-statement"); // Lab #8
 		}
-		System.out.println("Outside of the if-statement");
+		
+		System.out.println("Outside of the if-statement");  // Lab #7
+
 	}
 
 }
